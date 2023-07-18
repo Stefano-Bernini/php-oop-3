a249b9d48f49c7c65187716ca5a18804b4681b04
Stefano-Bernini <stefanobernini26@gmail.com>
2023-07-19 00:50:50 +0200

    creata, collegata e testata classe Email

---
+++ Models/Email.php
@@ -0,0 +1,41 @@
+<?php
+    class Email extends Comunicazioni{
+        public $allegato;
+        private $notifica_consegna;
+
+        public function __construct(String $_mittente, String $_destinatario, String $_oggetto, String $_contenuto, Bool $_notifica_consegna){
+            parent::__construct($_mittente, $_destinatario, $_oggetto, $_contenuto);
+            $this->notifica_consegna = $_notifica_consegna;
+        }
+
+        public function setAllegato($_allegato){
+            $this->allegato = $_allegato;
+        }
+
+        public function getAllegato(){
+            return $this->allegato;
+        }
+
+
+
+        public function setNotificaConsegna($_notifica_consegna){
+            $this->notifica_consegna = $_notifica_consegna;
+        }
+
+        public function getNotificaConsegna(){
+            return $this->notifica_consegna;
+        }
+
+        public function inoltro(){
+            return 'Email inoltrata';
+        }
+
+        public function stampa(){
+            return 'Email stampata';
+        }
+
+        public function invio(){
+            return 'Email inviata';
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     require_once __DIR__.'/Models/Comunicazioni.php';
+    require_once __DIR__.'/Models/Email.php';
 
     $comm = new Comunicazioni('Mario', 'Marco', 'Presentazione', 'Ciao sono Mario');
 
@@ -8,6 +9,26 @@
     echo "Oggetto: ".$comm->getOggetto().'<br/>';
     echo "Contenuto: ".$comm->getContenuto().'<br/>';
     echo "Stato invio: ".$comm->invio().'<br/>';
+
+
+    
+
+    $email = new Email('Fabio', 'Alessandro', 'Test email', 'Corpo della mail', true);
+
+    echo "<hr>";
+    echo "Mittente: ".$email->getMittente().'<br/>';
+    echo "Destinatario: ".$email->getDestinatario().'<br/>';
+    echo "Oggetto: ".$email->getOggetto().'<br/>';
+    echo "Contenuto: ".$email->getContenuto().'<br/>';
+    if($email->getNotificaConsegna()){
+        echo "L'email e' stata consegnata correttamente<br/>";
+    }
+    else{
+        echo "L'email non e' stata inviata, riprova<br/>";
+    }
+    echo "Stato invio: ".$email->invio().'<br/>';
+    echo "Inoltro: ".$email->inoltro().'<br/>';
+    echo "Stampa: ".$email->stampa().'<br/>';
 ?>
 
 <!DOCTYPE html>

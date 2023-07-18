59ab22ebf56e83b1326ab94abe8e12f6c6cf3563
Stefano-Bernini <stefanobernini26@gmail.com>
2023-07-19 01:18:58 +0200

    creata, collegata e testata classe Allegato

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     require_once __DIR__.'/Models/Comunicazioni.php';
     require_once __DIR__.'/Models/Email.php';
+    require_once __DIR__.'/Models/Allegato.php';
+
 
     $comm = new Comunicazioni('Mario', 'Marco', 'Presentazione', 'Ciao sono Mario');
 
@@ -11,7 +13,7 @@
     echo "Stato invio: ".$comm->invio().'<br/>';
 
 
-    
+
 
     $email = new Email('Fabio', 'Alessandro', 'Test email', 'Corpo della mail', true);
 
@@ -29,6 +31,29 @@
     echo "Stato invio: ".$email->invio().'<br/>';
     echo "Inoltro: ".$email->inoltro().'<br/>';
     echo "Stampa: ".$email->stampa().'<br/>';
+
+
+
+    $email_2 = new Email('Mattia', 'Nicola', 'Email per Nicola', 'Corpo della mail per Nicola', false);
+    $allegato = new Allegato('immagine', 'png', 1.8);
+
+    $email_2->setAllegato($allegato);
+
+    echo "<hr>";
+    echo "Mittente: ".$email_2->getMittente().'<br/>';
+    echo "Destinatario: ".$email_2->getDestinatario().'<br/>';
+    echo "Oggetto: ".$email_2->getOggetto().'<br/>';
+    echo "Contenuto: ".$email_2->getContenuto().'<br/>';
+    if($email_2->getNotificaConsegna()){
+        echo "L'email e' stata consegnata correttamente<br/>";
+    }
+    else{
+        echo "L'email non e' stata inviata, riprova<br/>";
+    }
+    echo "Stato invio: ".$email_2->invio().'<br/>';
+    echo "Inoltro: ".$email_2->inoltro().'<br/>';
+    echo "Stampa: ".$email_2->stampa().'<br/>';
+    echo "Allegato: ".$email_2->allegato->getNome().".".$email_2->allegato->getTipo().", Dimensioni: ".$email_2->allegato->getDimensione().'Mb<br/>';
 ?>
 
 <!DOCTYPE html>
